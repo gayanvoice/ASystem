7f1e2d448f558f105ebf486a7f4830d8eba93742
Gayan Kuruppu <gayan.kuruppu@live.com>
2022-12-23 20:36:07 +0000

    feat(singleton): add database singleton

---
+++ ASystem/Singleton/DatabaseSingleton.cs
@@ -0,0 +1,37 @@
+﻿using MySqlConnector;
+using System.IO;
+
+namespace ASystem.Singleton
+{
+    public sealed class DatabaseSingleton
+    {
+        private static DatabaseSingleton instance = null;
+        private static readonly object padlock = new object();
+
+        DatabaseSingleton()
+        {
+        }
+        public static DatabaseSingleton Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new DatabaseSingleton();
+                    }
+                    return instance;
+                }
+            }
+        }
+        public MySqlConnection MySqlConnection
+        {
+            get
+            {
+                string text = File.ReadAllText(@"C:\Users\Gayan\Desktop\key.txt");
+                return new MySqlConnection(text);
+            }
+        }
+    }
+}
